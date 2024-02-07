372456df67147bfa28f9a42c8ca691b81a3c1c15
Sergey Nagornykh <serega1905@ukr.net>
2024-02-07 18:34:46 +0200

    WidgetTop + typography + base mixins

--- src/components/suggestions/sidebar/WidgetTop.vue
+++ src/components/suggestions/sidebar/WidgetTop.vue
@@ -1,7 +1,33 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div class="widget-top">widget top</div>
+  <div class="widget-top">
+    <div class="widget-top__content">
+      <h2 class="h2 widget-top__title">Frontend Mentor</h2>
+      <h2 class="widget-top__subtitle">Feedback Board</h2>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.widget-top {
+  background: url('@/assets/suggestions/desktop/background-header.png') no-repeat center;
+  background-size: contain;
+  padding: 24px;
+  border-radius: 10px;
+  filter: drop-shadow(0 4px 4px rgba(0, 0, 0, 0.25));
+  height: 137px;
+  display: flex;
+  align-items: flex-end;
+
+  &__title {
+    color: var(--color-6);
+  }
+
+  &__subtitle {
+    color: var(--color-6);
+    opacity: 0.75;
+    font:
+      500 15px/1 'Jost',
+      sans-serif;
+  }
+}
+</style>
