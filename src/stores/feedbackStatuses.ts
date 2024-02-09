a9e9f620cc5efb37566dc13c03d36174fde6f987
Sergey Nagornykh <serega1905@ukr.net>
2024-02-09 17:39:29 +0200

    fix store feedbackStatuses reactivity issue

--- src/stores/feedbackStatuses.ts
+++ src/stores/feedbackStatuses.ts
@@ -1,4 +1,4 @@
-import { defineStore } from 'pinia'
+import { defineStore, storeToRefs } from 'pinia'
 import { useSuggestionsStore } from '@/stores/suggestions'
 import { computed } from 'vue'
 import type { IFeedbackStatus } from '@/types'
@@ -11,17 +11,21 @@ const COLORS_OF_STATUS = {
 }
 
 export const useFeedbackStatuses = defineStore('feedbackStatuses', () => {
-  const { suggestions } = useSuggestionsStore()
+  const { suggestions } = storeToRefs(useSuggestionsStore())
 
   const feedbackStatuses = computed<IFeedbackStatus[]>(() => {
-    const uniqStatuses = [...new Set(suggestions.map((item) => item.status))]
+    const uniqStatuses = [...new Set(suggestions.value.map((item) => item.status))]
 
-    return uniqStatuses.map((status) => ({
-      status,
-      label: status.charAt(0).toUpperCase() + status.slice(1),
-      color: COLORS_OF_STATUS[status],
-      amount: suggestions.filter((suggestion) => suggestion.status === status).length
-    }))
+    return uniqStatuses.map((status) => {
+      const amount = suggestions.value.filter((suggestion) => suggestion.status === status).length
+
+      return {
+        status,
+        amount,
+        label: status.charAt(0).toUpperCase() + status.slice(1),
+        color: COLORS_OF_STATUS[status]
+      }
+    })
   })
 
   return { feedbackStatuses }
